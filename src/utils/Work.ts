2b4fab1f1d756dfb6de696e8920ba5dc4f0b1653
Cryserrrrr <yoti3005@gmail.com>
2025-03-11 10:56:37 +0100

    add kcagenda

--- src/utils/Work.ts
+++ src/utils/Work.ts
@@ -1,15 +1,15 @@
-export type Work = { 
-  experiences: WorkItem[],
-  projects: WorkItem[],
-  shaders: WorkItem[]
-}
+export type Work = {
+  experiences: WorkItem[];
+  projects: WorkItem[];
+  shaders: WorkItem[];
+};
 
 export type WorkItem = {
-  title: string,
-  image: string,
-  link: string | null,
-  github: string
-}
+  title: string;
+  image: string;
+  link: string | null;
+  github: string | null;
+};
 
 const work: Work = {
   experiences: [
@@ -17,30 +17,37 @@ const work: Work = {
       title: "Solar System",
       image: "/images/solar-system.png",
       link: "https://solar-system-ecru-two.vercel.app/",
-      github: "https://github.com/Cryserrrrr/solarSystem"
+      github: "https://github.com/Cryserrrrr/solarSystem",
     },
   ],
   projects: [
     {
+      title: "KcAgenda",
+      image: "/images/kcagenda.png",
+      link: "https://kcagenda.com/",
+      github: null,
+    },
+    {
       title: "Openai",
       image: "/images/openai.png",
       link: null,
-      github: "https://github.com/Cryserrrrr/openAi-interface?tab=readme-ov-file"
+      github:
+        "https://github.com/Cryserrrrr/openAi-interface?tab=readme-ov-file",
     },
     {
       title: "Minecraft-CV",
       image: "/images/minecraft.png",
       link: "https://minecraft-cv.vercel.app/",
-      github: "https://github.com/Cryserrrrr/minecraftCv"
+      github: "https://github.com/Cryserrrrr/minecraftCv",
     },
     {
       title: "Portfolio",
       image: "/images/portfolio.png",
       link: null,
-      github: "https://github.com/Cryserrrrr/R3F-Portfolio"
-    }
+      github: "https://github.com/Cryserrrrr/R3F-Portfolio",
+    },
   ],
-  shaders: []
-}
+  shaders: [],
+};
 
-export default work
+export default work;
